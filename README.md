c35f6f54badf107054c0e51b19f258739f1dc190
Ari Zelanko <zelanko@gmail.com>
2016-12-11 15:07:10 -0500

    Return POST'd text as response. Add vscode launch config. Add express, eslint, eslint config file. Git to ignore node_modules

---
+++ README.md
@@ -0,0 +1,9 @@
+# SqlParse
+
+Webservice receive sql and return a parse or tokenization.
+
+## First milestones
+
+* Webservice receives request. Returns "Got it". \<Done\>
+* Post with SQL text returns same. \<Done\>
+* Returns tokenization or some parse artefact.
--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-var http = require("http");
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
 
-http.createServer(function (request, response) {
-   // Send the HTTP header 
-   // HTTP Status: 200 : OK
-   // Content Type: text/plain
-   response.writeHead(200, {'Content-Type': 'text/plain'});
-   
-   // Send the response body as "Hello World"
-   response.end('Got it!\n');
-}).listen(8081);
+app.use(bodyParser.text());
 
-// Console will print the message
-console.log('Server running at http://127.0.0.1:8081/');
+app.get('/', function(req, res){
+  res.send('Got it!');
+});
+
+app.post("/", function (req, res) {
+  res.send("server got: " + req.body || "Nothing.")
+  res.end();
+});
+
+app.listen(8081); 
