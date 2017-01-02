75d115fe3edb2e60586f30f8382a162b22ea294a
Ari Zelanko <zelanko@gmail.com>
2017-01-02 16:01:42 -0500

    Move antlr generated code to lib directory

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
+"strict mode";
 var antlr4 = require('antlr4');
-var tsqlLexer = require('./tsqlLexer');
-var tsqlParser = require("./tsqlParser");
-var tsqlListener = require("./tsqlListener");
-var sql = "select 'a dumb value' aS aString,123 as iNtvalUE;";
-var chars = new antlr4.InputStream(sql);
-var lexer = new tsqlLexer.tsqlLexer(chars);
-var tokens = new antlr4.CommonTokenStream(lexer);
-var parser = new tsqlParser.tsqlParser(tokens);
-parser.buildParseTrees = true
-var tree = parser.tsql_file();
-antlr4.tree.ParseTreeWalker.DEFAULT.walk(new tsqlListener.tsqlListener(), tree);
+console.log(process.cwd());
+var tsqlLexer = require('./lib/tsqlLexer');
+var tsqlParser = require("./lib/tsqlParser");
+var tsqlListener = require("./lib/tsqlListener");
+var customListener = new tsqlListener.tsqlListener();
+customListener.enterTsql_file = (ctx) => {
+  if(ctx && ctx.constructor && ctx.constructor.name) console.info("from %s", ctx.constructor.name);
+  if(ctx || ctx.children || ctx.children.length > 0) {
+    console.info("children: ", ctx.children.join(', '));
+  }   
+  console.info("enterTsql_file ");	    
+};
+customListener.enterSelect_statement = (ctx) => {
+  console.info("enterSelect_statement");
+};
+
+module.exports = {
+  format: function (sql) {
+    var chars = new antlr4.InputStream(sql);
+    var lexer = new tsqlLexer.tsqlLexer(chars);
+    var tokens = new antlr4.CommonTokenStream(lexer);
+    var parser = new tsqlParser.tsqlParser(tokens);
+    var formattedSql = "";
+    parser.buildParseTrees = true
+    var tree = parser.tsql_file();
+    antlr4.tree.ParseTreeWalker.DEFAULT.walk(customListener, tree);
+    return formattedSql;
+  }
+};
---
+++ spec/RecogniserSpec.js
@@ -0,0 +1,22 @@
+describe("Formatter", () => {
+  var expected,
+    formatter,
+    incoming;
+
+  incoming = 'select A as [Yuba dubba]';
+  expected = 'SELECT A AS [Yuba dubba];';
+  formatter = require('../index');
+
+  it("has a format function", () => {
+    expect(formatter).not.toBeUndefined();
+    expect(formatter.format).not.toBeUndefined();
+    expect(typeof formatter.format).toEqual("function");
+  });
+
+  it(`Formats '${incoming}' as '${expected}'`, () => {
+    var formattedSql = formatter.format(incoming);
+    expect(formattedSql).not.toBeUndefined();
+    expect(formattedSql).not.toBeNull();
+    expect(formattedSql).toEqual(expected);
+  });
+});
