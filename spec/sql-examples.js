528eefaa2ba52a887712419d29b9e464bb747fc5
Ari Zelanko <zelanko@gmail.com>
2017-01-07 16:46:52 -0500

    New select test scenario. Not passing yet.

--- spec/sql-examples.js
+++ spec/sql-examples.js
@@ -2,6 +2,7 @@
 module.loaded = false;
 module.exports.examples = [
   {incoming: 'select A as [Yuba dubba]', expected: 'SELECT A AS [Yuba dubba];'},
-  {incoming: 'select Column1, Column2 from ATable at', expected: "SELECT Column1,\n  Column2\nFROM ATable at;"}
+  {incoming: 'select Column1, Column2 from ATable at', expected: "SELECT Column1,\n  Column2\nFROM ATable at;"},
+  {incoming: 'SELECt all top 1 column1 from mYtAble at', expected: 'SELECT ALL TOP 1 column1\nFROM mYtAble at;'}
 ];
 module.loaded = true;
