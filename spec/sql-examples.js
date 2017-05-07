4dce3ba5e0a8f7b63b6ef1c7c0160797cd5d56fc
Ari Zelanko <zelanko@gmail.com>
2017-05-07 16:56:17 -0400

    Revert "New select test scenario. Not passing yet."
    This reverts commit 528eefaa2ba52a887712419d29b9e464bb747fc5.

--- spec/sql-examples.js
+++ spec/sql-examples.js
@@ -2,7 +2,6 @@
 module.loaded = false;
 module.exports.examples = [
   {incoming: 'select A as [Yuba dubba]', expected: 'SELECT A AS [Yuba dubba];'},
-  {incoming: 'select Column1, Column2 from ATable at', expected: "SELECT Column1,\n  Column2\nFROM ATable at;"},
-  {incoming: 'SELECt all top 1 column1 from mYtAble at', expected: 'SELECT ALL TOP 1 column1\nFROM mYtAble at;'}
+  {incoming: 'select Column1, Column2 from ATable at', expected: "SELECT Column1,\n  Column2\nFROM ATable at;"}
 ];
 module.loaded = true;
